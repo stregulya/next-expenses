3dd8824fd466b4ea03a2f511371fbe79b556cb0f
stregulya <d.polosuxin@gmail.com>
2025-10-11 00:59:53 +0700

    Добавлено использование react-hook-form для управления формой расходов и улучшен стиль компонентов формы

--- src/ui/ExpenseForm/ExpenseForm.tsx
+++ src/ui/ExpenseForm/ExpenseForm.tsx
@@ -1,24 +1,26 @@
 "use client";
 
-import { FormEvent, useEffect, useState } from "react";
 import styles from "./ExpenseForm.module.css";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+interface ExpenseForm {
+  amount: number;
+  category: string;
+  comment: string;
+}
 
 export default function ExpenseForm() {
-  const [amount, setAmount] = useState("");
+  const { register, handleSubmit, formState, reset } = useForm<ExpenseForm>({
+    mode: "onChange",
+  });
 
-  useEffect(() => {
-    const amount = localStorage.getItem("amount");
-    if (amount) console.log(`Сумма была: ${amount}`);
-  }, []);
-
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    localStorage.setItem("amount", amount);
-    setAmount("");
+  const onSubmit: SubmitHandler<ExpenseForm> = (data) => {
+    console.log(data);
+    reset();
   };
 
   return (
-    <form className={styles.form} onSubmit={handleSubmit}>
+    <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
       <h2 className={styles.title}>Расход</h2>
       <label htmlFor="amount" className={styles["input-title"]}>
         Сумма
@@ -26,18 +28,48 @@ export default function ExpenseForm() {
       <div className={styles["input-wrapper"]}>
         <input
           type="number"
-          name="amount"
-          id="amount"
+          min={0}
+          step={"any"}
           placeholder="100"
           className={styles.input}
-          value={amount}
-          onChange={(e) => setAmount(e.target.value)}
-          required
+          {...register("amount", {
+            required: true,
+            valueAsNumber: true,
+          })}
         />
       </div>
-      <button type="submit" className={styles["main-btn"]}>
-        Создать
-      </button>
+      <label htmlFor="amount" className={styles["input-title"]}>
+        Категория
+      </label>
+      <div className={styles["select-wrapper"]}>
+        <select className={styles.select} {...register("category", {})}>
+          <option value="products">Продукты</option>
+          <option value="vehicle">Транспорт</option>
+          <option value="cloth">Одежда</option>
+        </select>
+      </div>
+      <label htmlFor="amount" className={styles["input-title"]}>
+        Комментарий
+      </label>
+      <div className={styles["textarea-wrapper"]}>
+        <textarea
+          className={styles.textarea}
+          placeholder="Счастье не купишь. Но деньги могут помочь его обрести."
+          {...register("comment")}
+        ></textarea>
+      </div>
+      <div className={styles["btn-container"]}>
+        <button
+          type="submit"
+          className={styles["main-btn"]}
+          disabled={!formState.isValid}
+        >
+          Создать
+        </button>
+        <button className={styles["reset-btn"]} onClick={() => reset()}>
+          Очистить
+        </button>
+      </div>
     </form>
   );
 }
